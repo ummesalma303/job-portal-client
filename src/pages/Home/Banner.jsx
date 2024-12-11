12bfaab00ec84f36a0d3982c87264614181c90f1
ummesalma303 <ummesalma333999@gmail.com>
2024-12-11 17:02:56 +0600

    added banner and category card

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
-
+import { easeInOut, motion } from "motion/react"
+import team1 from '../../assets/team/team-1.jpg'
+import team2 from '../../assets/team/team-3.jpg'
 const Banner = () => {
     return (
         <div>
-            <div className="flex items-center bg-base-200 min-h-[80vh]">
-  <div className=" flex justify-between">
+            {/* <div className="flex items-center justify-between"> */}
+  <div className=" flex items-center space-x-28 bg-base-200 min-h-[70vh] px-6">
    
-    <div className='flex-1'>
-      <h1 className="text-5xl font-bold">The Easiest Way
-      to Get Your New Job</h1>
+    <div className='text-start w-[700px]'>
+      <motion.h1 animate={{x:[50,0] }}  transition={{duration:4,delay:1, ease: "easeInOut",repeat:Infinity }}   className="text-5xl font-bold">The Easiest Way
+      to Get Your New <motion.span animate={{color:["#bcbce1","#0c0c88"]}} transition={{duration:1.5, repeat:Infinity}}> Job</motion.span></motion.h1>
       <p className="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
       </p>
       <button className="btn btn-primary">Get Started</button>
     </div>
-    <div>
-     <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl flex-1" />
+    <div className=''>
+     <motion.img animate={{y:[50,100,50]} } transition={{duration:10,delay:1, ease:easeInOut, repeat:Infinity}}
+      src={team1}
+      className="max-w-sm rounded-bl-none rounded-[40px] shadow-2xl border-l-8 border-b-8 border-blue-700" />
+     <motion.img animate={{x:[100,150,100]} } transition={{duration:10,delay:1, ease:easeInOut, repeat:Infinity}}
+      src={team2}
+      className="max-w-sm rounded-bl-none rounded-[40px] shadow-2xl border-l-8 border-b-8 border-blue-700" />
       </div>
 
   </div>
 </div>
-        </div>
+        // </div>
     );
 };
 
---
+++ src/pages/Home/Category.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { motion } from "motion/react"
+import category1 from '../../assets/category/category-1.png'
+import category2 from '../../assets/category/category-2.png'
+import category3 from '../../assets/category/category-3.png'
+import category4 from '../../assets/category/category-4.png'
+const Category = () => {
+    return (
+       <div className="my-20">
+         <div className='text-center'>
+            <h2 className='text-4xl font-semibold'>Browse by category</h2>
+            <p>Find the job that’s perfect for you. about 800+ new jobs everyday</p>
+        </div>
+        
+        <div className="flex flex-wrap space-x-7 justify-center my-12">
+            <motion.span whileHover={{ scale: 1.1 }} transition={{ duration:0.5 }} className="flex items-center border-[1px] border-gray-400 hover:border-blue-300 max-w-fit rounded-lg shadow-none hover:shadow-lg p-4 space-x-2" >
+                <img src={category1} alt="" />
+                <div>
+                     <h2 className='text-xl font-semibold'>Marketing And Scale</h2>
+                     <p>1526 job available</p>
+                </div>
+            </motion.span>
+            <motion.span whileHover={{ scale: 1.1 }} transition={{ duration:0.5 }} className="flex items-center border-[1px] border-gray-400 hover:border-blue-300 max-w-fit rounded-lg shadow-none hover:shadow-lg p-4 space-x-2" >
+                <img src={category2} alt="" />
+                <div>
+                     <h2 className='text-xl font-semibold'>Marketing And Scale</h2>
+                     <p>1526 job available</p>
+                </div>
+            </motion.span>
+            <motion.span whileHover={{ scale: 1.1 }} transition={{ duration:0.5 }} className="flex items-center border-[1px] border-gray-400 hover:border-blue-300 max-w-fit rounded-lg shadow-none hover:shadow-lg p-4 space-x-2" >
+                <img src={category3} alt="" />
+                <div>
+                     <h2 className='text-xl font-semibold'>Marketing And Scale</h2>
+                     <p>1526 job available</p>
+                </div>
+            </motion.span>
+            <motion.span whileHover={{ scale: 1.1 }} transition={{ duration:0.5 }} className="flex items-center border-[1px] border-gray-400 hover:border-blue-300 max-w-fit rounded-lg shadow-none hover:shadow-lg p-4 space-x-2" >
+                <img src={category4} alt="" />
+                <div>
+                     <h2 className='text-xl font-semibold'>Marketing And Scale</h2>
+                     <p>1526 job available</p>
+                </div>
+            </motion.span>
+        </div>
+       </div>
+    );
+};
+
+export default Category;
