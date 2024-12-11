e9f4ffda2fd92a37f901bb492a7d149bbda142d6
ummesalma303 <ummesalma333999@gmail.com>
2024-12-11 07:39:15 +0600

    initial setup with nav and footer

---
+++ src/router/Router.jsx
@@ -0,0 +1,14 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+    },
+  ]);
+
+  export default router
